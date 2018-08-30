68586455c9ec49ed1ac2d913a16f4f91716cd589
Allis Tauri <allista@gmail.com>
2018-08-30 15:28:45 +0300

    Added KSP-AVC-updater

---
+++ KSP-AVC-updater/Program.cs
@@ -0,0 +1,23 @@
+﻿//   Program.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2016 Allis Tauri
+
+﻿using AT_Utils;
+
+namespace KSPAVCupdater
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			KSP_AVC_Updater.UpdateFor(new AT_Utils.ModInfo(), 
+			                          new AT_Utils.CCModInfo(),
+			                          new AtHangar.ModInfo(),
+			                          new ThrottleControlledAvionics.ModInfo(),
+			                          new GroundConstruction.ModInfo());
+		}
+	}
+}
