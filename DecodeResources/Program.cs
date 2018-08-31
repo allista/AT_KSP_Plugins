7da0ef8004b8956add9c071f6fe31e2485b5157e
Allis Tauri <allista@gmail.com>
2018-08-31 15:28:29 +0300

    TABS to SPACES

--- DecodeResources/Program.cs
+++ DecodeResources/Program.cs
@@ -16,65 +16,65 @@ using System.Text;
 
 namespace DecodeResources
 {
-	class MainClass
-	{
-		internal static byte[] read_stream(Stream stream)
-		{
-			byte b = (byte)stream.ReadByte();
-			b = (byte)~b;
-			for (int i = 1; i < 2; i++)
-				stream.ReadByte();
-			var array = new byte[stream.Length - stream.Position];
-			stream.Read(array, 0, array.Length);
-			if((b & 32) != 0)
-			{
-				for (int j = 0; j < array.Length; j++)
-					array[j] = (byte)~array[j];
-			}
-			return array;
-		}
+    class MainClass
+    {
+        internal static byte[] read_stream(Stream stream)
+        {
+            byte b = (byte)stream.ReadByte();
+            b = (byte)~b;
+            for (int i = 1; i < 2; i++)
+                stream.ReadByte();
+            var array = new byte[stream.Length - stream.Position];
+            stream.Read(array, 0, array.Length);
+            if((b & 32) != 0)
+            {
+                for (int j = 0; j < array.Length; j++)
+                    array[j] = (byte)~array[j];
+            }
+            return array;
+        }
 
-		internal static string get_string(byte[] res, int str)
-		{
-			int num;
-			if ((res[str] & 128) == 0)
-			{
-				num = (int)res[str];
-				str++;
-			}
-			else
-			{
-				if ((res[str] & 64) == 0)
-				{
-					num = ((int)res[str] & -129) << 8;
-					num |= (int)res[str + 1];
-					str += 2;
-				}
-				else
-				{
-					num = ((int)res[str] & -193) << 24;
-					num |= (int)res[str + 1] << 16;
-					num |= (int)res[str + 2] << 8;
-					num |= (int)res[str + 3];
-					str += 4;
-				}
-			}
-			if (num < 1) return string.Empty;
-			string @string = Encoding.Unicode.GetString(res, str, num);
-			return string.Intern(@string);
-		}
+        internal static string get_string(byte[] res, int str)
+        {
+            int num;
+            if ((res[str] & 128) == 0)
+            {
+                num = (int)res[str];
+                str++;
+            }
+            else
+            {
+                if ((res[str] & 64) == 0)
+                {
+                    num = ((int)res[str] & -129) << 8;
+                    num |= (int)res[str + 1];
+                    str += 2;
+                }
+                else
+                {
+                    num = ((int)res[str] & -193) << 24;
+                    num |= (int)res[str + 1] << 16;
+                    num |= (int)res[str + 2] << 8;
+                    num |= (int)res[str + 3];
+                    str += 4;
+                }
+            }
+            if (num < 1) return string.Empty;
+            string @string = Encoding.Unicode.GetString(res, str, num);
+            return string.Intern(@string);
+        }
 
-		public static void Main(string[] args)
-		{
-			int str = 0;
-			if(args.Length > 0) 
-				int.TryParse(args[0], out str);
-			var assembly = Assembly.GetAssembly(typeof(Vessel));
-			using(Stream stream = assembly.GetManifestResourceStream("Assembly-CSharpAssembly-CSharp"))
-			{
-				var res = read_stream(stream);
-				Console.WriteLine(get_string(res, str));
-			}
-		}
-	}
+        public static void Main(string[] args)
+        {
+            int str = 0;
+            if(args.Length > 0) 
+                int.TryParse(args[0], out str);
+            var assembly = Assembly.GetAssembly(typeof(Vessel));
+            using(Stream stream = assembly.GetManifestResourceStream("Assembly-CSharpAssembly-CSharp"))
+            {
+                var res = read_stream(stream);
+                Console.WriteLine(get_string(res, str));
+            }
+        }
+    }
 }
